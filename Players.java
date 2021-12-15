44d2c5645303768c0f3d290feeddb165058a1803
19pa1a0564 <60499483+19pa1a0564@users.noreply.github.com>
2021-12-15 10:12:12 +0530

    Create Players.java

---
+++ Players.java
@@ -0,0 +1,9 @@
+class Players {
+    private String playerName;
+    Players(String playerName) {
+        this.playerName = playerName;  
+    }
+    public String  getPlayerName(){
+      return playerName;
+    } 
+}
