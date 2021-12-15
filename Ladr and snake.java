f9707f9e0faeb95ca33188abd04db7261f30cc3a
19pa1a0564 <60499483+19pa1a0564@users.noreply.github.com>
2021-12-15 10:11:35 +0530

    Create Ladr and snake.java

---
+++ Ladr and snake.java
@@ -0,0 +1,8 @@
+class Ladrandsnake {
+    int startPoint;
+    int endPoint;
+    public Ladrandsnake(int startPoint, int endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
